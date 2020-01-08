2f3b30fd9099d60040795915a0e4e7559543f1c1
charles <charlestsai@tw.ibm.com>
2020-01-08 15:57:10 +0800

    add ariel grammar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Highlight, { defaultProps, Prism } from "prism-react-renderer";
+import {ArielGrammar} from './ariel-grammar';
+
+const exampleCode = `
+select * from tablename;
+update a where a=123
+`;
 
 function App() {
+  // console.log(Prism.languages.sql);
+  Prism.languages.ariel = ArielGrammar;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Highlight {...defaultProps} code={exampleCode} language="ariel">
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <pre className={className} style={style}>
+        {tokens.map((line, i) => (
+          <div {...getLineProps({ line, key: i })}>
+            {line.map((token, key) => (
+              <span {...getTokenProps({ token, key })} />
+            ))}
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
+
       </header>
     </div>
   );
